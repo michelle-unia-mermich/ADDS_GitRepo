e51626c24574a74df8f43fb80af48acbabeb828f
michelle-unia-mermich <michelle.nguyen@student.adelaide.edu.au>
2024-09-01 19:53:02 +0930

    update

--- main.cpp
+++ main.cpp
@@ -11,15 +11,10 @@ int main()
     RecursiveBinarySearch recursiveBinarySearch;
 
     //Create a main function that takes in a list of integers (one line, separated by space) int1 int2 int3 ... intn.
-    //!Question: should the users key in the number of elements they want to be in the array?
-    //!or will they continue typing in until they use a specific key to end the typing of input values?
+    //!Question: how to type inputs in one line that terminates when the user presses enter? is my way the right way - to detect the enter key?
 
-    std::cout<<"Key in number of elements in the array, then key in the elements: ";
-    int n;
-    std::cin>>n;
-    
     std::vector<int> input;
-    for(int i=0;i<n;i++)
+    while (std::cin.get() != '\n')
     {
         int temporary_element;
         std::cin>>temporary_element;
