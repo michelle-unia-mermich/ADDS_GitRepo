4103eee544fb68ae5f38466f73323e05083bbb75
michelle-unia-mermich <michelle.nguyen@student.adelaide.edu.au>
2024-09-08 23:58:44 +0930

    a

--- main.cpp
+++ main.cpp
@@ -1,29 +1,30 @@
 #include <iostream>
-#include "Referee.h"
-#include "Player.h"
-#include "Human.h"
-#include "Computer.h"
+#include "linkedlist.h"
 
-int main()
-{
-    //create the referee
-    Referee referee1;
-    Player* player1;
-    Player* player2;
-    player1 = new Human("Anna");
-    player2 = new Computer();
-    Player* result_ptr= referee1.refGame(player1, player2);
-    if (result_ptr!=nullptr)
-    {
-        std::cout<< result_ptr->getName()<< " Wins"<<std::endl;
-    }
-    else
-    {
-        std::cout<< "It's a Tie."<<std::endl;
-    }
+using namespace std;
 
-    delete player1;
-    delete player2;
+int main() {
+    // build and initialize a linked list object named ll
+    LinkedList ll;
+
+    ll.insertAtPosition(9, 0);    // insert a node into an empty list
+    ll.insertAtPosition(3, 0);    // insert a node infront of the only node
+    ll.insertAtPosition(6, 1);    // insert a node between our two nodes
+    ll.insertAtPosition(12, 10);  // insert a node out of bounds
+    ll.insertAtPosition(12, 3);   // insert a node at the end of the list
+
+    // traverse and print the linked list
+    ll.printList();               // prints [3, 6, 9]
+    std::cout << "---" << std::endl;
+
+    // traverse to specific nodes (0, 1, 2, 3) and print the data at these (handles out of bounds access)
+    for (unsigned int i = 0; i < 4; i++) {
+        Node* target = ll.traverse(i);
+        if (target != nullptr)
+            std::cout << target->data << std::endl;
+        else
+            std::cout << "there is no data here, target = nullptr" << std::endl;
+    }
 
     return 0;
 }
